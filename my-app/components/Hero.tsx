3cb64d4a87a85c35a5fd89847b239ffd528c1d2f
Piotr <1brozek.peter@gmail.com>
2025-08-04 17:55:46 -0500

    info changes

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
 
             <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless User Experiences"/>
 
-            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">👋 Hi, I&apos;m Peter, a Software Engineer based in Chicago.</p>
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">👋 Hi, I&apos;m Piotr, a Software Engineer based in Chicago.</p>
 
             <a href="#projects">
             <MagicButton 
--- my-app/data/index.ts
+++ my-app/data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently contributing to SAGE3 Software",
+    title: "Currently learning more about Machine Learning and AI",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -70,8 +70,25 @@ export const gridItems = [
 ];
 
 export const projects = [
+  
   {
     id: 1,
+    title: "MRI Brain Tumor Detection and Classification ",
+    des: "A machine learning-based system for detecting and classifying brain tumors from MRI scans to assist in early and accurate diagnosis.",
+    img: "/mlProject.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "Vite.png", "chakratransp.png"],
+    link: "",
+  },
+  {
+    id: 2,
+    title: "Real-Time Transcription App",
+    des: "A real-time transcription app that converts spoken language into text instantly using speech-to-text technology for enhanced accessibility and productivity.",
+    img: "/mic.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "Vite.png", "chakratransp.png"],
+    link: ""
+  },
+  {
+    id: 3,
     title: "AI Powered Stock Dashboard",
     des: "Unlock smarter investing as intelligent AI analyzes data in real-time to recommend the best stocks for your portfolio.",
     img: "/project1.png",
@@ -79,29 +96,29 @@ export const projects = [
     link: "https://github.com/pbroz35/Stocks-project",
   },
   {
-    id: 2,
+    id: 4,
     title: "AI Chatbot",
     des: "Experience seamless conversations with our AI chatbot, designed to understand and respond to your needs in real-time.",
     img: "/project2Edit2.jpg",
     iconLists: ["chakratransp.png", "/tail.svg", "/ts.svg", "Vite.png"],
     link: "https://github.com/pbroz35/AI-Assistant",
   },
-  {
-    id: 3,
-    title: "Movie Library",
-    des: "Discover your next favorite film with our movie website. Browse an extensive library, access detailed movie information",
-    img: "/project3.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "Vite.png"],
-    link: "https://github.com/pbroz35/movie-website",
-  },
-  {
-    id: 4,
-    title: "OpenStreetMap",
-    des: "A mapping system developed in C++ to find your way around campus by using Dijkstra's algorithm",
-    img: "/b5.svg",
-    iconLists: ["axios.png", "ISO_C++_Logo.svg"],
-    link: "https://github.com/pbroz35",
-  },
+  // {
+  //   id: 3,
+  //   title: "Movie Library",
+  //   des: "Discover your next favorite film with our movie website. Browse an extensive library, access detailed movie information",
+  //   img: "/project3.png",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "Vite.png"],
+  //   link: "https://github.com/pbroz35/movie-website",
+  // },
+  // {
+  //   id: 4,
+  //   title: "OpenStreetMap",
+  //   des: "A mapping system developed in C++ to find your way around campus by using Dijkstra's algorithm",
+  //   img: "/b5.svg",
+  //   iconLists: ["axios.png", "ISO_C++_Logo.svg"],
+  //   link: "https://github.com/pbroz35",
+  // },
 ];
 
 export const testimonials = [
@@ -112,7 +129,7 @@ export const testimonials = [
     title: "Software Engineer Intern at Electronic Visualization Lab",
   },
   {
-    quote: "Peter is an excellent worker and is always diligent in his tasks. He helps whenever he can, and never fails to show up on time, ready to work. He listens to others, and works with them to not only improve himself, but the group as a whole. He is an amazing friend, and a pleasure to work with!",
+    quote: "Piotr is an excellent worker and is always diligent in his tasks. He helps whenever he can, and never fails to show up on time, ready to work. He listens to others, and works with them to not only improve himself, but the group as a whole. He is an amazing friend, and a pleasure to work with!",
     name: "Adyan Khan",
     title: "Former Colleague",
   }
@@ -153,17 +170,25 @@ export const companies = [
 
 export const workExperience = [
 
-  {
+    {
     id: 2,
-    title: "Undergraduate Researcher",
-    desc: "Collaborated with the software engineering team in weekly meetings to develop and enhance the SAGE3 software using TypeScript, JavaScript, React, and Docker, and assisted in implementing UI and AI features under the guidance of Research Professor Luc Renambot.",
+    title: "Research Assistant - Electronic Visualization Lab",
+    desc: "Enhanced SAGE3, a collaborative whiteboard used by 5,000+ users across 800+ institutions, by implementing LLM features such as DeepPlot visualizations, speaker diarization, and Llama 3 summarization using TypeScript, React, LangChain, and Docker. Built AI-powered export tools with Python and React to generate summaries as CSVs, PDFs, images, and sticky notes.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Fullstack Engineer Intern",
-    desc: "Developed and maintained multiple web applications, utilizing modern technologies such as ViteJS, React, TailwindCSS, NodeJS, and TypeScript to create responsive and dynamic user experiences.",
+    title: "Software Engineer Intern - Ingenio Healthcare",
+    desc: "As a Full-Stack Developer Intern at Ingenio Healthcare, I built a physician-finder tool using React, Redux, Node.js, and PostgreSQL, integrating NPPES data and MapGL for seamless provider search and scheduling. I also developed an AI-powered insurance authorization system with the OpenAI API, reducing approval time by 30% and enabling physicians to write authorizations 25% faster.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp1.svg",
+  },
+
+  {
+    id: 4,
+    title: "Fullstack Engineer Intern - Electronic Visualization Lab",
+    desc: "As a Full-Stack Software Intern at the Electronic Visualization Lab, I improved the performance of an AI-powered stock dashboard by 25% through custom web scraping and data filtering from 100+ sources per request. I built real-time data applications with React, Node.js, and Express, integrating AI for predictive stock insights. I also developed interactive tools like a movie website and AI chatbot to boost user engagement.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
